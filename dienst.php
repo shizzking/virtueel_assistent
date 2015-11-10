2c8979cd13fc0ec9e8881df9be804d92b5850c74
Bob Ruis <bob.ruis@live.nl>
2015-11-10 20:40:46 +0100

    20:40

--- dienst.php
+++ dienst.php
@@ -26,21 +26,29 @@ include_once 'config.php';
                         die("Connection failed: " . $db->connect_error);
                     }
                         $sql = "SELECT company_id, name, description FROM company";
-                        $sql_categorie = "SELECT * FROM users LEFT JOIN company ON users.company_id = company.company_id WHERE user_id = '$id'";
+                        $query = "SELECT * FROM company LEFT JOIN diensten on categorie_ID = diensten.ID";
+//                        $sql_c = "SELECT * FROM users LEFT JOIN company ON users.company_id = company.company_id WHERE user_id = '$id'";
+                        $result_2 = $db->query($query);
                         $result = $db->query($sql);
 
                     if ($result->num_rows > 0) {
                         // output data of each row
                         while($row = $result->fetch_assoc()) {
                             echo "<table border=1>";
-                            echo "<tr><td class='op'>ID: </td><td>" . $row["company_id"]. "</td></tr>";
-                            echo "<tr><td class='op'>Name: </td><td>" . $row["name"]. "</td></tr>";
+                            echo "<tr><td>ID: </td><td>" . $row["company_id"]. "</td></tr>";
+                            echo "<tr><td>Name: </td><td>" . $row["name"]. "</td></tr>";
                             echo "<tr><td>Description: </td><td>" . $row["description"]. "</td></tr>";
-                            
+                            echo "<tr><td>Sector: </td><td></td></tr>";
                             echo "</table>";
                         }
                     } else {
                         echo "0 results";
+                    }
+                    
+                    if($result_2->num_rows> 0){
+                        while ($row_2 = $result_2->fetch_assoc()){
+                            echo $row_2['sector'];
+                    }
                     }
                     $db -> close();
                 ?>    
